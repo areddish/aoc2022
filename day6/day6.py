9d030d910894cfe45ddabb573132cc5afe0961be
Andrew Reddish <areddish@foosion.net>
2022-12-06 13:29:47 -0500

    Day 6

--- day6/day6.py
+++ day6/day6.py
@@ -7,22 +7,22 @@ tests = [
 ]
 
 def find_marker(message, marker_length=4):
-    start = 0
+    cur = 0
     i = marker_length
-    while True:
-        if len(set(message[start:i])) == marker_length:
-            return i
-        start += 1
-        i += 1
-
+    while cur < len(message) - marker_length:
+        end = cur+marker_length
+        if len(set(message[cur:end])) == marker_length:
+            return end
+        cur += 1
 
 # Tests to validate cases
 for t in tests:
     assert find_marker(t[0]) == t[1], t[0]
 
+# Solution
 with open("day6.txt") as file:
     day6_data = [x.strip() for x in file.readlines()][0]
 
-    print("Part 1 = ", find_marker(day6_data, 4))
-    print("Part 2 = ", find_marker(day6_data, 14))
+    print("Part 1 =", find_marker(day6_data, 4))
+    print("Part 2 =", find_marker(day6_data, 14))
 
