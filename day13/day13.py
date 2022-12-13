32c6daaca6ef1c5ba7d98830328e2c3b671a6dfc
Andrew Reddish <areddish@foosion.net>
2022-12-13 02:53:52 -0500

    Day 13

--- day13/day13.py
+++ day13/day13.py
@@ -1,24 +1,23 @@
-def compare_items(left, right):
-    if isinstance(left, list) and isinstance(right, list):
-        return compare_packets(left, right)
-    elif isinstance(left, int) and isinstance(right, int):
-        return left < right
-    else:
-        if isinstance(left, list):
-            return compare_packets(left, [right])
-        else:
-            assert isinstance(right, list)
-            return compare_packets([left], right)
-            
-    assert False
-
 def compare_packets(p1, p2, d=0):
     for i in range(len(p1)):
         if i >= len(p2):
             return False
-        if p1[i] == p2[i]:
+        # Either need to have tri-state comparison lt,eq,gt or can compare the entire packets.
+        left = p1[i]
+        right = p2[i]
+        if left == right:
             continue
-        return compare_items(p1[i], p2[i])
+        if isinstance(left, list) and isinstance(right, list):
+            return compare_packets(left, right)
+        elif isinstance(left, int) and isinstance(right, int):
+            # Equality here will hose the solution, it's now handled above.
+            return left < right
+        else:
+            if isinstance(left, list):
+                return compare_packets(left, [right])
+            else:
+                assert isinstance(right, list)
+                return compare_packets([left], right)
     return True
 
 #with open("test.txt") as file:
@@ -54,11 +53,8 @@ with open("day13.txt") as file:
         ordered_packets.insert(index, packet)
 
     # Find divider packets and multiply
-    divider_1_index = 0
-    divider_2_index = 0
+    p2 = 1
     for index,packet in enumerate(ordered_packets):
-        if packet == DIVIDER_1:
-            divider_1_index = index + 1 
-        elif packet == DIVIDER_2:
-            divider_2_index = index + 1
-    print("Part 2 =", divider_1_index * divider_2_index)
+        if packet == DIVIDER_1 or packet == DIVIDER_2:
+            p2 *= index + 1 
+    print("Part 2 =", p2)
