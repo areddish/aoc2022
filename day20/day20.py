6f05fcb1a994a90b7f259c867f56ca4ddac29097
Andrew Reddish <areddish@foosion.net>
2022-12-20 01:48:02 -0500

    Day 20 - Final

--- day20/day20.py
+++ day20/day20.py
@@ -1,5 +1,3 @@
-import copy
-
 class Node:
     def __init__(self, val):
         self.val = val
@@ -9,121 +7,100 @@ class Node:
     def __str__(self) -> str:
         return str(self.val)
 
-def index(i, offset, limit):
-    if offset > 0:
-        return (i + offset) % limit
-    else:
-        for i in range(offset, 0):
-            i += offset
-            if i < 0:
-                i = limit-1
-        return i
+    def create_doubly_linked_list(data):
+        for i in range(1,len(data)-1):
+            data[i].left = data[i-1]
+            data[i].right = data[i+1]
+
+        data[0].left = data[-1]
+        data[0].right = data[1]
+        data[-1].left = data[-2]
+        data[-1].right = data[0]
+        return data
+
+def print_list(head):
+    print(str(head), end=",")
+    cur = head.right
+    while cur != head:
+        print(str(cur), end=",")
+        cur = cur.right
+    print()
+
+DECRYPTION_KEY = 811589153
 
 #with open("test.txt") as file:
 with open("day20.txt") as file:
     nums = [int(x.strip()) for x in file.readlines()]
-    data = [Node(x) for x in nums]
-
-    assert len(data) == len(nums)   
+    data_p1 = [Node(x) for x in nums]
+    data_p2 = [Node(x*DECRYPTION_KEY) for x in nums]
     p1 = 0
     p2 = 0
 
-    # create list
-    for i in range(1,len(data)-1):
-        data[i].left = data[i-1]
-        data[i].right = data[i+1]
+    # Create a doubly linked list from the data in the list. We keep two arrays because
+    # data maintains both a reference inside the doubly linked list to the node we want move
+    # as well as the original order to move in.
+    data_p1 = Node.create_doubly_linked_list(data_p1)
+    data_p2 = Node.create_doubly_linked_list(data_p2)
 
-    data[0].left = data[-1]
-    data[0].right = data[1]
-    data[-1].left = data[-2]
-    data[-1].right = data[0]
+    def mix(data, times=1):
+        for _ in range(times):
+            for node_to_move in data:
+                # Nothing to do if 0
+                if node_to_move.val == 0:                    
+                    continue
 
-    head = data[0]
-    def print_list(head):
-        print(str(head), end=",")
-        cur = head.right
-        while cur != head:
-            print(str(cur), end=",")
-            cur = cur.right
-        print()
+                # # For printing conveninece
+                # if head == node_to_move:
+                #     head = node_to_move.right
 
-    # def find_list(head, n):
-    #     cur = head
-    #     while cur.val != n:
-    #         cur = cur.right
-    #     return cur
+                #print(node_to_move.val, ": (",node_to_move,")", end="")
+                #print_list(head)
 
-    for node_to_move in data:
-        if node_to_move.val == 0:
-            print("not moving zero")
-            continue
-        #node_to_move = find_list(head, n)
-        if head == node_to_move:
-            head = node_to_move.right
+                # remove node_to_move
+                node_to_move.left.right = node_to_move.right
+                node_to_move.right.left = node_to_move.left
+                #print_list(head)
 
-        #print(node_to_move.val, ": (",node_to_move,")", end="")
-        #print_list(head)
-        
-        #find node to insert
-        offset = -1 if node_to_move.val < 0 else 1
-        node_dest = node_to_move
-        for _ in range(abs(node_to_move.val)+(1 if offset == -1 else 0)):
-            if offset == -1:
-                # skip the node still in the list
-                if node_dest.left == node_to_move:
-                    node_dest = node_dest.left
-                node_dest = node_dest.left
-            else:                
-                # skip the node still in the list
-                if node_dest.right == node_to_move:
-                    node_dest = node_dest.right
-                node_dest = node_dest.right
-        
-        # remove node_to_move
-        node_to_move.left.right = node_to_move.right
-        node_to_move.right.left = node_to_move.left
-        #print_list(head)
-        
-        # re-insert
-        node_to_move.right = node_dest.right
-        node_to_move.left = node_dest
-        node_dest.right.left = node_to_move
-        node_dest.right = node_to_move 
-        #print_list(head)
+                offset = -1 if node_to_move.val < 0 else 1
+                # Start to the left or right of the node we removed, and subtract off one from the # of times we'll go left/right.
+                # also MOD the total distance by the # of node - 1 to reduce the number of cycles we perform for part 2.
+                # The offset of +1 if going left is because the insertion code below always adds it after the node_dest
+                node_dest = node_to_move.left if node_to_move.val < 0 else node_to_move.right
+                for _ in range((abs(node_to_move.val)-1+(1 if offset == -1 else 0))%(len(data)-1)):
+                    if offset == -1:
+                        node_dest = node_dest.left
+                    else:                
+                        node_dest = node_dest.right
+                # re-insert
+                node_to_move.right = node_dest.right
+                node_to_move.left = node_dest
+                node_dest.right.left = node_to_move
+                node_dest.right = node_to_move 
+                #print_list(head)
+    #    print_list(head)
+        return data
 
-
-        # n_index = answer.index(n)
-        # dest_index = index(n_index, n, len(answer))
-
-        # offset = 1 if n > 0 else -1
-        # for i in range(abs(n)):
-        #     next_index = index(n_index, offset, len(answer))
-        #     temp = answer[n_index]
-        #     answer[n_index] = answer[next_index]
-        #     answer[next_index] = temp
-        #     n_index = next_index
-        # print(" => " , answer)
-    print_list(head)
-
-    zero = next(filter(lambda x: x.val == 0, data))
-
-    i = 0
-    while i < 3001:
-        if i == 1000 or i == 2000 or i == 3000:
-            print(zero.val)
-            p1 += zero.val
-        zero = zero.right
-        i += 1
+    def score(data):
+        ans = 0
+        zero = next(filter(lambda x: x.val == 0, data))
+        i = 0
+        while i < 3001:
+            if i == 1000 or i == 2000 or i == 3000:
+                #print(zero.val)
+                ans += zero.val
+            zero = zero.right
+            i += 1
+        return ans
     
-    # answer = [1, 2, -3, 4, 0, 3, -2]
-    # print(answer)
-
-    # zero_index = answer.index(0)    
-    # print(answer[index(zero_index, 1000, len(answer))])
-    # print(answer[index(zero_index, 2000, len(answer))])
-    # print(answer[index(zero_index, 3000, len(answer))])
-    print("Part 1 =", p1)    
-    print("Part 2 =", p2)
+    print("Part 1 =", score(mix(data_p1, 1)))    
+    print("Part 2 =", score(mix(data_p2, 10)))
 
 #12433 too low!
 #12574 to low!
+
+# p2
+# 1623178306 too low (yeah, oops I was submitting the test answer :( )
+# 1623178306
+# 1623178306
+# 4869534918 too low
+# Ans: 4789999181006
